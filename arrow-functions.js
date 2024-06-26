16a04a41c2cc41012a138a4812f31c73b633dca4
Eden Essame <eden_essame80@hotmail.com>
2024-06-26 15:38:31 +0000

    arrow func

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,38 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(5, 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(5, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b; 
+
+let sum2 = addTwoNumbers2(10, 5);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Hello there");
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string
+
+    </p>`
+)
+console.log(returnMultipleLines());
